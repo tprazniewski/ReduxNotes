12f11c2eb15fc5fc635f76023e973a65e36f3b4c
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-07-05 00:53:51 +0200

    Create action and dispatch

--- redux1.js
+++ redux1.js
@@ -13,8 +13,12 @@ const store = createStore((state = {count: 0} ) {
 store.getState() // we get the value from tthe store in this case a defaultt object which is {count: 0}
 
 // Create Actions
+    {
+      //uppercase by convention  if we had more worrds we would say 'INCREMENT_ANOTHER'
+      type: 'INCREMENT'
+    }
 
-      {
-        //uppercase by convention
-        type: 'INCREMENT'
-      }
+    // If we would like to send action to the store we use dispatch
+
+    store.dispatch({  type: 'INCREMENT'})
+    // dispatch actually allow us to send of an action object so store can do sth with this info
